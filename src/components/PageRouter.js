deca8af0dbd2acff351605048613dd0e85227f77
Balaagha <b.alihummatov@gmail.com>
2020-04-04 19:30:07 +0300

    finish all

--- src/components/PageRouter.js
+++ src/components/PageRouter.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import {Route,Switch} from 'react-router-dom'
+import {Route,Switch,Link} from 'react-router-dom'
 import Create from "./Create/Create";
+import Edit from "./Create/Edit"
 import Actual from "./Actual";
 import Archive from "./Archive";
 import SingleNoteCard from "./SingleNoteCard/SingleNoteCard";
 const PageRouter = ({setNoteList,noteList}) => {
+    const errorLoader =()=>{
+        return(
+            <div className={"error-container"}>
+                <h1 className={"error-h1"} >Something gone wrong</h1>
+                <Link to={'/'}>Bact To Home</Link>
+            </div>
+        )
+    }
     return (
         <>
             <Switch>
@@ -12,6 +21,9 @@ const PageRouter = ({setNoteList,noteList}) => {
                 <Route path={"/archive"} ><Archive setNoteList={setNoteList} noteList={noteList} /></Route>
                 <Route path={"/create"} ><Create setNoteList={setNoteList} noteList={noteList} /></Route>
                 <Route path={"/notes/:id"} ><SingleNoteCard setNoteList={setNoteList} noteList={noteList} /></Route>
+                <Route path={"/edit/:id"} ><Edit setNoteList={setNoteList} noteList={noteList} /></Route>
+                <Route path={"*"} exac render={errorLoader} />
+
             </Switch>
         </>
     );
